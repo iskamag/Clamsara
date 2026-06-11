6bce8ed0f9b1524f69a4fc1ef5c822d57a67a4dc
iska <iska@iskamag.com>
2026-06-12 01:08:10 +0500

    wipe

--- run-tests.lisp
+++
@@ -1,4 +0,0 @@
-(load (merge-pathnames #p"~/quicklisp/setup.lisp"
-                        (truename #p"~/")))
-(push #p"/home/iskam/src/vibe/Clamsara/" asdf:*central-registry*)
-(asdf:test-system :clamsara/test)
